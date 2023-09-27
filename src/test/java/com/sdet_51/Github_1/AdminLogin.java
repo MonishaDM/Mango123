74474944b07a93367cf231ed2759b79d8af784d6
srksi <srksi@LAPTOP-RP61NQ22>
2023-09-27 16:05:35 +0530

    Deleted and Added admin login

---
+++ src/test/java/com/sdet_51/Github_1/AdminLogin.java
@@ -0,0 +1,11 @@
+package com.sdet_51.Github_1;
+
+import org.testng.annotations.Test;
+
+public class AdminLogin {
+
+	@Test
+	public void adminLogin() {
+		System.out.println("----- Login as Admin -----");
+	}
+}
